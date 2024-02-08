7641ad5dbc43b5f306a732238463ab1e62655f01
Stefan <mohrst84031@th-nuernberg.de>
2024-02-08 17:39:41 +0100

    WIP fix prisma bug in vercel

--- src/root.tsx
+++ src/root.tsx
@@ -18,6 +18,7 @@ export default component$(() => {
 
   console.log("Hello from QwikCity!")
   console.log(process.env.NODE_ENV)
+  console.log(process.env.VITE_VERCEL_ENV)
   console.log(process.env.TEST)
   console.log(process.env.VITE_DATABASE_URL)
   console.log(process.env.VITE_VERCEL_DATABASE_URL)
