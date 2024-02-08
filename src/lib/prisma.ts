ae431e3b90ee191aa622df919ce707c971f8f8b4
Stefan <mohrst84031@th-nuernberg.de>
2024-02-08 14:03:50 +0100

    WIP fix prisma bug in vercel

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,7 +1,14 @@
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
 
-const prisma = new PrismaClient().$extends(withAccelerate())
+
+const prisma = new PrismaClient({
+    datasources: {
+        db: {
+            url: process.env.POSTGRES_PRISMA_URL
+        }
+    }
+}).$extends(withAccelerate())
 
 
 export default prisma;
